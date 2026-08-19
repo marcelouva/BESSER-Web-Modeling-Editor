da45dda89f01d480198893ef555ea635ad9a88fd
marcelo <uva@dc.exa.unrc.edu.ar>
2026-08-19 10:16:19 -0300

    Change Sat Consistency Check by Semantic Check

--- packages/webapp/src/main/app/shell/menus/TopBarUtilities.tsx
+++ packages/webapp/src/main/app/shell/menus/TopBarUtilities.tsx
@@ -120,7 +120,7 @@ export const TopBarUtilities: React.FC<TopBarUtilitiesProps> = ({
         Syntactic Check
       </DropdownMenuItem>
       <DropdownMenuItem onClick={() => { void onConsistencyCheck(); }}>
-        Sat Consistency Check
+        Semantic  Check
       </DropdownMenuItem>
     </DropdownMenuContent>
   </DropdownMenu>
